0de090ff72d67a64b7602834589a06978d6a90aa
theideabased <ogunmusireseyi01@gmail.com>
2024-10-03 13:40:46 +0000

    start coding for coins needed for total to be sum of coins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 """Given a pile of coin with different values print out
 the fewest number needed to give and amount"""
+
+
+def makechange(coins, total):
+    """ coin is a list and how main element of the coin will
+    be added to get the total
+    makechange(coin, total)
+    Return: 0 if total is 0
+    -1 if sum of coin is greater than total
+    -1 if no amount of coin element will give total"""
+    sorted_coin = sorted(coins, asc=True)
+    if total = 0:
+        return 0
+    for coin in range(len(sorted_coin)):
+        list_total = list_total + sorted_coins[coin]
+        if list_total >= total:
+            return -1
+        elif list_total == total:
+            return len(sorted_coin)
+
