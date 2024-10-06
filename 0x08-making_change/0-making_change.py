3bf5e29e5e6bf83d80c0cfd61bce4745c1c5dc58
theideabased <ogunmusireseyi01@gmail.com>
2024-10-06 17:39:51 +0000

    making the final changes of the code'

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python3
-"""Given a pile of coin with different values print out
-the fewest number needed to give and amount"""
+"""Module to determine the fewest number of coins needed to meet a given total."""
 
 
-def makechange(coins, total):
-    """ coin is a list and how main element of the coin will
-    be added to get the total
-    makechange(coin, total)
-    Return: 0 if total is 0
-    -1 if sum of coin is greater than total
-    -1 if no amount of coin element will give total"""
-    sorted_coin = sorted(coins, asc=True)
-    if total = 0:
+def makeChange(coins, total):
+    """
+    Determine the fewest number of coins needed to meet a given total.
+    
+    Args:
+    coins (list): A list of the coin denominations available.
+    total (int): The total amount to reach with the fewest coins.
+    
+    Returns:
+    int: Fewest number of coins needed to reach the total, or -1 if it cannot be done.
+    """
+    if total <= 0:
         return 0
-    for coin in range(len(sorted_coin)):
-        list_total = list_total + sorted_coins[coin]
-        if list_total >= total:
-            return -1
-        elif list_total == total:
-            return len(sorted_coin)
+    
+    # Sort coins in descending order to use the largest coin first
+    coins.sort(reverse=True)
+    
+    coin_count = 0  # To keep track of how many coins are used
+    for coin in coins:
+        if total == 0:
+            break  # If total becomes zero, stop
+        
+        # Use as many of this coin as possible
+        num_coins = total // coin
+        coin_count += num_coins
+        total -= num_coins * coin  # Reduce the total by the value of used coins
 
+    # If after all this, the total isn't 0, it means we couldn't match the exact total
+    if total != 0:
+        return -1
+    
+    return coin_count
