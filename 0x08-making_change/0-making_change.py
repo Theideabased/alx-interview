9d695bd534313066a09cfe5065209ca6b816e4fe
theideabased <ogunmusireseyi01@gmail.com>
2024-10-06 17:53:19 +0000

    making my file pycodestyle respected

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
-"""Module to determine the fewest number of coins needed to meet a given total."""
+"""Module to determine the fewest number of coins needed to meet a given
+total."""
 
 
 def makeChange(coins, total):
     """
     Determine the fewest number of coins needed to meet a given total.
-    
+
     Args:
     coins (list): A list of the coin denominations available.
     total (int): The total amount to reach with the fewest coins.
-    
+
     Returns:
-    int: Fewest number of coins needed to reach the total, or -1 if it cannot be done.
+    int: Fewest number of coins needed to reach the total, or -1 if it
+    cannot be done.
     """
     if total <= 0:
         return 0
-    
+
     # Sort coins in descending order to use the largest coin first
     coins.sort(reverse=True)
-    
+
     coin_count = 0  # To keep track of how many coins are used
     for coin in coins:
         if total == 0:
             break  # If total becomes zero, stop
-        
+
         # Use as many of this coin as possible
         num_coins = total // coin
         coin_count += num_coins
-        total -= num_coins * coin  # Reduce the total by the value of used coins
+        total -= num_coins * coin
 
-    # If after all this, the total isn't 0, it means we couldn't match the exact total
+    # If after all this, the total isn't 0, it means we couldn't match the
+    # exact total
     if total != 0:
         return -1
-    
+
     return coin_count
