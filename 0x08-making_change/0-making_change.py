2bd90320e3cde0f0c91d6926286d15da3fdf3f0c
Theideabased <ogunmusireseyi01@gmail.com>
2024-11-28 19:20:37 +0000

    solve the change coin problem using bubble osrt and dynamic programming

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Module to determine the fewest number of coins needed to meet a given
 total."""
+from typing import List
 
 
-def makeChange(coins, total):
+def makeChange(coins: List[int], total:int):
     """
     Determine the fewest number of coins needed to meet a given total.
 
@@ -15,25 +16,18 @@ def makeChange(coins, total):
     int: Fewest number of coins needed to reach the total, or -1 if it
     cannot be done.
     """
-    if total <= 0:
-        return 0
-
-    # Sort coins in descending order to use the largest coin first
-    coins.sort(reverse=True)
-
-    coin_count = 0  # To keep track of how many coins are used
-    for coin in coins:
-        if total == 0:
-            break  # If total becomes zero, stop
-
-        # Use as many of this coin as possible
-        num_coins = total // coin
-        coin_count += num_coins
-        total -= num_coins * coin
-
-    # If after all this, the total isn't 0, it means we couldn't match the
-    # exact total
-    if total != 0:
-        return -1
-
-    return coin_count
+    coin_length = len(coins)
+    for i in range(coin_length):
+        for j in range(0, coin_length - i - 1):
+            if coins[j] > coins[j + 1]:
+                coins[j], coins[j+1] = coins[j+1], coins[j]
+    used_coins = []
+    n = len(coins)
+    i = n - 1
+    while (i >= 0):
+        while (total >= coins[i]):
+            total -= coins[i]
+            used_coins.append(coins[i])
+        i -= 1
+    
+    return(len(used_coins))
